dca79ed5475d7a9869b5a899891eba6af839ae62
unknown <kcavner@hotmail.com>
2023-03-28 17:12:54 -0500

    removed passport util

--- utils/auth.js
+++ utils/auth.js
@@ -1,14 +1,5 @@
 
-<<<<<<< HEAD
-=======
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
-  
->>>>>>> 04cf8962975a5fc0e3e9af4302ffbf6ac9b1bc33
+
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the request to the login route
   if (!req.session.logged_in) {
