bd1095ac001f828a16c93acf443a9c6f360492b6
unknown <kcavner@hotmail.com>
2023-03-28 17:05:14 -0500

    got topic handlebars working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+// const userRoutes = require('./userRoutes');
 const forumpostRoutes = require('./forumpostRoutes');
 
-router.use('/userRoutes', userRoutes);
-router.use('/forumpostRoutes', forumpostRoutes);
+// router.use('/user', userRoutes);
+router.use('/forumpost', forumpostRoutes);
 
 module.exports = router;
--- public/js/topics.js
+++ public/js/topics.js
@@ -1,18 +1,18 @@
 
-  const topicLinks = document.querySelectorAll('.topic');
+//   const topicLinks = document.querySelectorAll('.topic');
 
-topicLinks.forEach((link) => {
-  link.addEventListener('click', async (event) => {
-    event.preventDefault();
+// topicLinks.forEach((link) => {
+//   link.addEventListener('click', async (event) => {
+//     event.preventDefault();
 
-    const href = link.getAttribute('href');
-    const response = await fetch(`/api/Forumpost${href}`);
+//     const href = link.getAttribute('href');
+//     const response = await fetch(`/api/Forumpost${href}`);
 
-    if (response.ok) {
-      const html = await response.text();
-      document.getElementById('content-container').innerHTML = html;
-    } else {
-      alert('Failed to load content');
-    }
-  });
-});
+//     if (response.ok) {
+//       const html = await response.text();
+//       document.getElementById('content-container').innerHTML = html;
+//     } else {
+//       alert('Failed to load content');
+//     }
+//   });
+// });
